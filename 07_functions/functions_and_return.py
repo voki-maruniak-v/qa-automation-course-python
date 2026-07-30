98e4aff1e05556b3b0b100a5cdb197f92751246b
PLAYNET\maruniak-v <vadym.maruniak@vokigames.com>
2026-07-30 17:20:01 +0200

    Первая практика с функциями: их вызов и return

---
+++ 07_functions/functions_and_return.py
@@ -0,0 +1,8 @@
+def sum_nums(a, b):
+    sum = a + b
+    return sum
+
+first_sum = sum_nums(10, 5)
+print(first_sum)
+
+print(sum_nums(50.5, 20))
---
+++ 07_functions/functions_basics.py
@@ -0,0 +1,6 @@
+def hello(name):
+    print("Hello there!", name)
+    print("Hi there!", name)
+
+hello('Vadym')
+hello('Alex')
