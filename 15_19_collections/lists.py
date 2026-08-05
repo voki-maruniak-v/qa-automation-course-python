af946338ba0132f164e58e64e1b57200a3d66b6d
PLAYNET\maruniak-v <vadym.maruniak@vokigames.com>
2026-08-05 20:30:36 +0200

    Python: add lists practice

---
+++ 15_19_collections/lists.py
@@ -0,0 +1,15 @@
+first_list = [1, 'abc', True, 2.5, False]
+
+first_list.pop(2)
+
+print(len(first_list))
+
+first_list.reverse()
+
+print(first_list)
+
+second_list = [24, 'Vadya']
+
+first_list.extend(second_list)
+
+print(first_list)
